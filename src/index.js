4605ba3bd8ba14d40ca6b3a5409211f39aea13f0
elpnt <quantumular@gmail.com>
2020-05-02 20:28:10 +0900

    finished

---
+++ src/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import visit from 'unist-util-visit';
+import split from 'split-on-first';
+import { markdownRenderer } from 'inkdrop';
+
+const parseTitle = (options) => (tree) => {
+  visit(
+    tree,
+    (node) => node.type === 'code',
+    (node, index, parent) => {
+      const [lang, title] = split(node.lang, ':');
+      if (title || title == '') {
+        node.lang = lang;
+        const titleNode = {
+          type: 'html',
+          value: `<div className="code-title">${title}</div>`,
+        };
+        parent.children.splice(index, 0, titleNode);
+        console.log(node);
+      }
+    }
+  );
+};
+
+module.exports = {
+  activate() {
+    markdownRenderer.remarkPlugins.push(parseTitle);
+  },
+  deactivate() {},
+};
