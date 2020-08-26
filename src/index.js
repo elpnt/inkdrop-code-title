64bdb4345d6fe740ded37e48a03111bff74b2073
elpnt <quantumular@gmail.com>
2020-08-26 22:36:24 +0900

    update: allow spaces in a title

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 'use strict';
 
-import visit from 'unist-util-visit';
-import split from 'split-on-first';
 import { markdownRenderer } from 'inkdrop';
-
-const Entities = require('html-entities').AllHtmlEntities;
-const entities = new Entities();
-
-const parseTitle = (options) => (tree) => {
-  visit(
-    tree,
-    (node) => node.type === 'code',
-    (node, index, parent) => {
-      let [lang, title] = split(node.lang || '', ':');
-      if (title || title === '') {
-        node.lang = lang;
-        title = entities.encode(title);
-        const titleNode = {
-          type: 'html',
-          value: `<div className="code-title">${title}</div>`,
-        };
-        parent.children.splice(index, 0, titleNode);
-      }
-    }
-  );
-};
+import parseTitle from './title-parser';
 
 module.exports = {
   activate() {
     markdownRenderer.remarkPlugins.push(parseTitle);
   },
-  deactivate() {},
 };
---
+++ src/title-parser.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import visit from 'unist-util-visit';
+import split from 'split-on-first';
+import { AllHtmlEntities as Entities } from 'html-entities';
+
+const parseTitle = (options) => (tree) => {
+  const entities = new Entities();
+
+  //
+  // unist-util-visit fucntion parses the below code
+  //
+  //   ```js:This is a hello function
+  //   fucntion hello() {
+  //     console.log('hello');
+  //   }
+  //   ```
+  //
+  // and convert this to an Object
+  //   {
+  //     lang: "js:This",
+  //     meta: "is a hello function",
+  //     value: "function hello() {\n  console.log('hello');\n}",
+  //     type: "code",
+  //     position: ...
+  //   }
+  //
+
+  visit(
+    tree,
+    (node) => node.type === 'code',
+    (node, index, parent) => {
+      let [lang, title] = split(node.lang || '', ':');
+      let meta = node.meta || '';
+      if (title || title === '') {
+        node.lang = lang;
+        title = entities.encode(title + ' ' + meta);
+        const titleNode = {
+          type: 'html',
+          value: `<div className="code-title">${title}</div>`,
+        };
+        parent.children.splice(index, 0, titleNode);
+      }
+    }
+  );
+};
+
+export default parseTitle;
