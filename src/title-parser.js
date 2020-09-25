972a4dbd025e2df08a26d9acd5d8dc179cdbf45b
elpnt <quantumular@gmail.com>
2020-09-26 01:16:08 +0900

    option to insert a title in a code block

--- src/title-parser.js
+++ src/title-parser.js
@@ -1,46 +1,58 @@
-'use strict';
+"use strict";
 
-import visit from 'unist-util-visit';
-import split from 'split-on-first';
-import { AllHtmlEntities as Entities } from 'html-entities';
+import visit from "unist-util-visit";
+import split from "split-on-first";
+import { AllHtmlEntities as Entities } from "html-entities";
+
+//
+// unist-util-visit fucntion parses the below code
+//
+//   ```js:This is a hello function
+//   fucntion hello() {
+//     console.log('hello');
+//   }
+//   ```
+//
+// then yields a Node object
+//   {
+//     lang: "js:This",
+//     meta: "is a hello function",
+//     value: "function hello() {\n  console.log('hello');\n}",
+//     type: "code",
+//     position: ...
+//   }
+//
 
 const parseTitle = (options) => (tree) => {
   const entities = new Entities();
-
-  //
-  // unist-util-visit fucntion parses the below code
-  //
-  //   ```js:This is a hello function
-  //   fucntion hello() {
-  //     console.log('hello');
-  //   }
-  //   ```
-  //
-  // and convert this to an Object
-  //   {
-  //     lang: "js:This",
-  //     meta: "is a hello function",
-  //     value: "function hello() {\n  console.log('hello');\n}",
-  //     type: "code",
-  //     position: ...
-  //   }
-  //
+  const position = inkdrop.config.get("code-title.titlePosition").toLowerCase();
 
   visit(
     tree,
-    (node) => node.type === 'code',
+    (node) => node.type === "code",
     (node, index, parent) => {
-      let [lang, title] = split(node.lang || '', ':');
-      const meta = node.meta || '';
-      if (title || title === '') {
+      let [lang, title] = split(node.lang || "", ":");
+      const meta = node.meta || "";
+
+      // preserve mdast-node info in data field
+      node.data = node.data || {};
+      node.data.hProperties = node.data.hProperties || {};
+
+      if (title || title === "") {
         node.lang = lang;
-        if (meta) title += ' ' + meta;
+        if (meta) title += " " + meta;
         title = entities.encode(title);
         const titleNode = {
-          type: 'html',
-          value: `<div class="code-title">${title}</div>`,
+          type: "html",
+          value: `<div class="code-title ${position}">${title}</div>`,
         };
         parent.children.splice(index, 0, titleNode);
+
+        let langClass = "";
+        if (lang) langClass = "language-" + lang;
+        node.data.hProperties.className = node.data.hProperties.className || [];
+        node.data.hProperties.className.push(langClass);
+        node.data.hProperties.className.unshift("with-title-" + position);
       }
     }
   );
