3f9e0eb18af2fa3c637653937a295047bcb2a56d
Matsuoka Kensuke <quantumular@gmail.com>
2025-04-12 14:39:03 +0900

    fix styles

--- src/title-parser.js
+++ src/title-parser.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import visit from "unist-util-visit";
 
 const { fontFamily: defaultFontFamily } = inkdrop.config.defaultSettings.editor;
@@ -7,12 +5,16 @@ const { fontFamily: customFontFamily } = inkdrop.config.settings.editor;
 const fontFamily = customFontFamily || defaultFontFamily;
 
 function escapeHtml(str) {
-  return str
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
+	return str
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+}
+
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 // unist-util-visit fucntion parses the below code
@@ -34,35 +36,35 @@ function escapeHtml(str) {
 //   }
 
 export default function parseTitle() {
-  return (tree) => {
-    visit(
-      tree,
-      (node) => node.type === "code" && !node.hasCodeTitle,
-      (node, index, parent) => {
-        const lang = node.lang;
-        const meta = node.meta || "";
-        let title = node.data.hProperties.title;
+	return (tree) => {
+		visit(
+			tree,
+			(node) => node.type === "code" && !node.hasCodeTitle,
+			(node, index, parent) => {
+				const lang = node.lang;
+				const meta = node.meta || "";
+				let title = node.data.hProperties.title;
 
-        // preserve mdast-node info in data field
-        node.data = node.data || {};
-        node.data.hProperties = node.data.hProperties || {};
+				// preserve mdast-node info in data field
+				node.data = node.data || {};
+				node.data.hProperties = node.data.hProperties || {};
 
-        if (title || title === "") {
-          node.hasCodeTitle = true;
-          if (meta) title += " " + meta; // Allow for a space in the title
-          title = escapeHtml(title);
-          if (title === "") title = lang; // If title is empty, fallbak to lang
+				if (title || title === "") {
+					node.hasCodeTitle = true;
+					if (meta) title += " " + meta; // Allow white space in titles
+					title = escapeHtml(title);
+					if (title === "") title = capitaRize(lang); // If title is empty, fallback to lang
 
-          const titleNode = {
-            type: "html",
-            value: fontFamily
-              ? `<div class="code-title" style="font-family: ${fontFamily};">${title}</div>`
-              : `<div class="code-title">${title}</div>`,
-          };
+					const titleNode = {
+						type: "html",
+						value: fontFamily
+							? `<div class="code-title" style="font-family: ${fontFamily};">${title}</div>`
+							: `<div class="code-title">${title}</div>`,
+					};
 
-          parent.children.splice(index, 0, titleNode);
-        }
-      }
-    );
-  };
+					parent.children.splice(index, 0, titleNode);
+				}
+			},
+		);
+	};
 }
